c415bf3f5d606dae93fb691dbe894c3b75b4e999
Loubna EL GUEDDARI <loubna.elgueddari@gmail.com>
2019-04-25 10:15:50 +0200

    [pep8]: fixing pep8 problems

--- pysap/plugins/mri/reconstruct_3D/linear.py
+++ pysap/plugins/mri/reconstruct_3D/linear.py
@@ -115,9 +115,9 @@ class pyWavelet3(object):
             the reconstructed data.
         """
         if self.coeffs_shape is None:
-            raise ValueError("The attributes coeffs_shape must be instanciated",
-                             "you can do it by callin linear_op.op with the",
-                             " right dimension")
+            raise ValueError("The attributes coeffs_shape must be",
+                             " instanciated you can do it by calling",
+                             " linear_op.op with the right dimension")
         self.coeffs = coeffs
         coeffs_dict = self.unflatten(coeffs, self.coeffs_shape,
                                      multichannel=self.multichannel)
