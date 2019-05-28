fc590491106ad7662a460610db1941d766d5b9f5
Loubna EL GUEDDARI <loubna.elgueddari@gmail.com>
2019-05-28 15:07:21 +0200

    Adding virtual coils

--- pysap/plugins/mri/parallel_mri/utils.py
+++ pysap/plugins/mri/parallel_mri/utils.py
@@ -19,6 +19,44 @@ This module contains all the utils tools needed in the p_MRI reconstruction.
 # Third party import
 import numpy as np
 
+def virtual_coil_reconstruction(imgs):
+    """
+    Calculate the combination of all the coils thanks to the virtual coil
+    methods as defined:
+
+    Parameters
+    ----------
+    imgs: np.ndarray
+        The images reconstructed channel by channel [Nch, Nx, Ny, Nz]
+
+    Returns
+    -------
+    I: np.ndarray
+        The combination of all the channels in a complex valued [Nx, Ny, Nz]
+    """
+    # Compute first the virtual coil
+    nch, nx, ny, nz = imgs.shape
+    weights = np.sum(np.abs(imgs), axis=0)
+    weights[weights==0] = 1e-16
+    phase_reference = np.asarray([np.angle(np.sum(
+        imgs[ch].flatten())) for ch in range(nch)])
+    reference = np.asarray([(imgs[ch] / weights) / np.exp(1j * phase_reference[ch])
+                           for ch in range(32)])
+    virtual_coil = np.sum(reference, axis=0)
+    difference_original_vs_virtual = np.asarray(
+        [np.conjugate(imgs[ch]) * virtual_coil for ch in range(nch)])
+    hanning_1d = np.expand_dims(np.hanning(np.minimum(nx,ny)), 1)
+    hanning_2d = np.fft.fftshift(np.dot(hanning_1d, hanning_1d.T))
+    if nz == 1:
+        hanning_Nd = np.expand_dims(np.tile(hanning_2d, (nch, 1, 1)), -1)
+    else:
+        hanning_Nd = np.tile(hanning_2d, (nch, 1, 1, nz))
+    # Removing the background noise via low pass filtering
+    difference_original_vs_virtual = np.fft.ifft2(np.fft.fft2(
+        difference_original_vs_virtual, axes=(1, 2)) * hanning_Nd, axes=(1, 2))
+    I = np.asarray([imgs[ch] * np.exp(1j * np.angle(
+                    difference_original_vs_virtual[ch])) for ch in range(nch)])
+    return np.sum(I, 0)
 
 def check_lipschitz_cst(f, x_shape, lipschitz_cst, max_nb_of_iter=10):
     """
